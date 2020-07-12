ff26ccc20c95be21b59da3e5389aa13ec4a91189
Ji Hyeok <48278519+landturtler@users.noreply.github.com>
2020-07-12 21:17:08 +0900

    Update 1149.cpp

--- 1149/1149.cpp
+++ 1149/1149.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-
-int go(int num, int col, vector<vector<int> > color, vector<vector<int> > &v) {
+int go(int num, int col, vector<vector<int> > color, vector<vector<int> > &v) { 
 	if(v[num][col] != INF) return v[num][col];
 	if (num == 0 ) return 0; 
 	v[num][col] = min(go(num-1,(col+1)%3,color,v),
@@ -15,7 +14,7 @@ int go(int num, int col, vector<vector<int> > color, vector<vector<int> > &v) {
 }
 
 int solution(int n, vector<vector<int> > &color) {
-	vector< vector<int> > v( n+1,vector<int>(3,INF));
+	vector< vector<int> > v( n+1,vector<int>(3,INF)); //v[num][col] : num-1번째 집까지 칠하고, num번째 집을 col색으로 칠하는데 발생하는 최소 비용
 	return min(min(go(n,0,color,v),go(n,1,color,v)),go(n,2,color,v));
 }
 
@@ -26,7 +25,7 @@ int main() {
 	int n;
 	cin >> n;
 	int t;
-	vector<vector<int>>color(n+1,vector<int>(3));
+	vector<vector<int>>color(n+1,vector<int>(3));//color[num][col] : num번째 집을 col색으로 칠하는 비용
 	for(int i=1;i<=n;i++) {
 		for(int j=0;j<3;j++) {
 			cin >>t;
